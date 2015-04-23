0d9c951bd0780cec0d99b52b853fc20ff7610bbd
uhahn <uhahn@opsi4.ub.hsu-hh.de>
2015-04-23 13:49:27 +0200

    working: login as readonly user alert a failed login (!) request a default list request a list for a given ClientId

--- swinv.js
+++ swinv.js
@@ -2,37 +2,73 @@
     var app = angular.module('opsi-swinv', []);
   
     app.controller('swinvController',function($http,$filter){
-	this.products=opsiswp.result;
+
+//	this.products=opsiswp.result;
 	this.baseurl="/interface?{%20%22id%22:%201,%20%22method%22:%20%22auditSoftware_getObjects%22,%20%22params%22:%20[]%20}";
-	this.baseurl='/interface';
+	baseurl='/rpc';
+	logonurl='https://opsireader:opsipass@'+window.location.host+'/rpc';
 	this.testurl='/interface?{"id":1,"method":"auditSoftware_getObjects","params":[]}';
 
-	this.auditParams={'id':1,'method':'auditSoftware_getObjects','params':[]};
+	this.auditParams={'id':'jsonrpc','method':'auditSoftwareOnClient_getObjects','params':[]};
+	this.logonParams={'id':'1','method':'authenticated','params':[]};
 
 	this.auditParams.params.push([]);
-	this.auditParams.params.push({"clientId":"dings"});
+//	this.auditParams.params.push({"clientId":"0024e81bc792.ub.hsu-hh.de"});
+	this.auditParams.params.push({"clientId":"782bcbb49fd9.ub.hsu-hh.de"}); // goihl
 
-//	this.getObj={'url':this.baseurl,'params':this.auditParams}; // gets mangled
+	this.getObj={'method':'jsonp','url':baseurl,'params':this.auditParams}; // gets mangled
 
-	this.opsiURL=this.baseurl+"?"+$filter('json')(this.auditParams);
+
+	opsiURL=baseurl+"?"+$filter('json')(this.auditParams);
+
+	me=this;
+
+	this.logon=function(){
+	    $http.post(logonurl,this.logonParams)
+		.success(function(response){
+//		    alert($filter('json')(response));
+		})
+		.error(function(response){
+		    alert("err: "+(response));
+		});
+	};
 
 	this.getprods=function(id){
-	    this.opsires="calling.. with "+this.opsiURL;
-	    $http.get(this.opsiURL).
-		success(function(response,stat,head,conf){
-		    this.opsires=response;
-		    this.opsistat=stat;
-		}).
-		error(function(err,stat,head,conf){
-		    this.opsires=err;
-		    this.opsistat=stat;
+	    this.opsires="calling.. "+id;
+	    if(id)this.auditParams.params[1].clientId=id;
+//	    this.opsistat=$filter('json')(this.getObj);
+	    $http.post(baseurl,this.auditParams)
+		.success(function(response,stat,head,conf){
+		   // alert("success: "+response);
+		    me.opsires="success";
+		    if(response.error)alert(response.error.message);
+		    me.products=response.result;
+		    me.opsistat=stat;
+		    me.opsihead=head;
+		    me.opsiconf=conf;
+		})
+		.error(function(err,stat,head,conf){
+		    me.opsires="error";
+		    alert("err: "+(err)+stat+head+$filter('json')(conf));
+		    me.opsistat=stat;
+		    me.opsihead=head;
+		    me.opsiconf=conf;
 		});
+//	    this.opsihead="call done";
 	    
 	};
 
+	this.logon();
 //	this.getprods();
 
-	
+	this.nonMS=function(name){
+//	    return true;
+	    if(name.match(/KB[0-9]+/))  return false;
+	    if(name.match(/Microsoft/)) return false;
+	    if(name.match(/^IE/)) return false;
+	    return true;
+	}
+
 	this.getfirst=function(){
 	    this.opsires="init";
 //	    alert(this.opsires);
